9afb107829fae7923a773d8bf00e0caa9682a2c4
julijavan <juli.javanainen@metropolia.fi>
2025-03-20 13:50:11 +0200

    moduuli 9

---
+++ moduuli-9/tehtava9.4.py
@@ -0,0 +1,37 @@
+import random
+
+
+class Auto:
+    def __init__(self, rtunnus, huippunopeus):
+        self.rtunnus = rtunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+    def accelerate(self, nopeudenmuutos):
+        self.nopeus += nopeudenmuutos
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        elif self.nopeus < 0:
+            self.nopeus = 0
+
+    def kuljettu(self, aika):
+        self.matka += aika * self.nopeus
+
+autot = []
+for i in range(10):
+    huippunopeus = random.randint(100, 200)
+    autot.append(Auto(f'ABC-{i}', huippunopeus))
+
+winnerfound = False
+
+while not winnerfound:
+    for auto in autot:
+        auto.accelerate(random.randint(-10, 15))
+        auto.kuljettu(1)
+        if auto.matka >= 10000:
+            winnerfound = True
+
+autot.sort(key=lambda a: a.matka)
+for auto in autot:
+           print(f'{auto.rtunnus}, {auto.huippunopeus}', {auto.nopeus}, {auto.matka})
